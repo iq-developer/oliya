6533a663129b81ad5549493a6d1a15de66c36759
Vladimir Sevastianov <108.nimay@gmail.com>
2021-03-28 22:58:58 +0300

    init: start oliya shop project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './assets/logo.png';
+import menu from './assets/menu.png';
+import cart from './assets/cart.png';
+import database from './oliya-db.json';
+
+const Tr = ({prices}) => {
+  return Object.entries(prices).map(([priceKey, priceValue]) => {
+    return (
+      <tr key={priceKey}>
+        <td className="volume">{priceKey}<small> мл:</small></td>
+        <td className="price"><strong>{priceValue}</strong><small> грн</small></td>
+        <td className="form">
+          <form>
+            <button>-</button><input size="1" placeholder=" 0" /><button>+</button>
+          </form>
+        </td>
+      </tr>
+    )
+  });
+}
+
+const Block = () => {
+  return Object.entries(database.products).map(([key, value]) => {
+    return (
+      <article className="block" key={key}>
+        <div className="title">
+          <img src={value.img} alt={`${value.title}`} className="float" />
+          <h2>{value.category} <br /><strong>{value.title}</strong></h2>
+        </div>          
+        <table className="table">
+          <tbody>
+            <Tr prices={value.price} /> 
+          </tbody>
+        </table>
+      </article>
+    );
+})};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <nav>
+        <div className="inner">
+          <a href="#"><img src={logo} className="logo" alt="Лого" /></a>
+          <a href="#"><img src={menu} className="menu" alt="Меню" /></a>
+          <form>
+            <input type="search" className="search" size="1" placeholder="Пошук по сайту" />
+          </form>
+        </div>
+      </nav>
+
+      <header>
+        <div className="inner">
+          <div className="phone" hidden>
+            <a href="tel:+380959300302">+38(095)930-03-02</a>
+          </div>
+          <div className="cart">
+            <form>
+              <button className="white-button"><img src={cart} alt="Корзина замовлення" /></button>
+            </form>
+            <div className="amount">товаров: <strong>2</strong> шт</div>
+            <div className="sum">на сумму: <strong>123</strong> грн</div>
+          </div>
+        </div>
       </header>
-    </div>
+
+      <main>
+        <Block />     
+      </main>
+    </>
   );
 }
 
