2207bae6e5cb4fd1d708c0288818760466001c7c
Vladimir Sevastianov <108.nimay@gmail.com>
2021-04-09 10:08:58 +0300

    feat(Order): user fields

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,15 +2,15 @@ import { useState } from 'react';
 
 const Order = () => {
 
-  const [delivery, setDelivery] = useState('Нова пошта');
-  const [deliveryOption, setDeliveryOption] = useState('Нова пошта');
-
   const option = "Нова пошта";
   const option1 = "Самовивіз з офісу (пр. Чорновола 63)";
   const option2 = "Самовивіз з кафе Грін (вул. Братів Рогатинців, 5)";
   const option3 = 'На відділення Нової Пошти';
   const option4 = "Курь'єрська доставка за адресою";
-  const option5 = '';
+
+  const [delivery, setDelivery] = useState(option);
+  const [deliveryOption, setDeliveryOption] = useState(option3);
+  const [user, setUser] = useState({});
 
   const handleDeliveryChange = (event) => {
     setDelivery(event.target.value);
@@ -18,58 +18,112 @@ const Order = () => {
   const handleDeliveryOptionChange = (event) => {
     setDeliveryOption(event.target.value);
   }
+  // универсальный хендлер для всех значений пользователя 
+  const handleChange = (event) => {
+    setUser(prevState => {
+      return {...prevState, [event.target.name]: event.target.value}
+    });
+  }
 
   return (
     <article className="block-order">
-          <form>
-            <h2>Спосіб доставки {delivery}</h2>
-            <input
-              name="delivery"
-              type="radio"
-              value={option}
-              checked={delivery === option}
-              onChange={handleDeliveryChange}
-            /> {option} <br />
-            <input
-              name="delivery"
-              type="radio"
-              value={option1}
-              checked={delivery === option1}
-              onChange={handleDeliveryChange}
-            /> {option1} <br />
-            <input
-              name="delivery"
-              type="radio"
-              value={option2}
-              checked={delivery === option2}
-              onChange={handleDeliveryChange}
-            /> {option2} <br /><br />
+      <form>
+        <h2>Спосіб доставки</h2>
+        <input
+          name="delivery"
+          type="radio"
+          value={option}
+          checked={delivery === option}
+          onChange={handleDeliveryChange}
+        /> {option}<br />
+        <input
+          name="delivery"
+          type="radio"
+          value={option1}
+          checked={delivery === option1}
+          onChange={handleDeliveryChange}
+        /> {option1}<br />
+        <input
+          name="delivery"
+          type="radio"
+          value={option2}
+          checked={delivery === option2}
+          onChange={handleDeliveryChange}
+        /> {option2}<br /><br />
 
-            <input /> Ім'я отримувача <br />
-            <input /> Призвіще отримувача <br />
-            <input /> Телефон отримувача <br />
+        <input
+          name="name"
+          value={user.name}
+          onChange={handleChange}
+          />
+        <label> Ім'я отримувача<br /></label>
+        <input
+          name="familyName"
+          value={user.familyName}
+          onChange={handleChange}
+          />
+        <label> Призвіще отримувача<br /></label>
+        <input
+          name="phone"
+          value={user.phone}
+          onChange={handleChange}
+          />
+        <label> Телефон отримувача<br /></label>
 
-            <h3>Відправляти на відділення або замовити курьерську доставку за вашою адресою?</h3>
+        <div hidden={delivery !== option}>
+          <h3>Відправляти на відділення або курьерськой доставкою за вашою адресою?</h3>
 
-            <input
-              name="deliveryOption"
-              type="radio"
-              value={option3}
-              checked={deliveryOption === option3}
-              onChange={handleDeliveryOptionChange}
-            /> {option4} <br />
-            <input
-              name="deliveryOption"
-              type="radio"
-              value={option4}
-              checked={deliveryOption === option4}
-              onChange={handleDeliveryOptionChange}
-            /> {option4} <br /><br />
+          <input
+            name="deliveryOption"
+            type="radio"
+            value={option3}
+            checked={deliveryOption === option3}
+            onChange={handleDeliveryOptionChange}
+          /> {option3} <br />
+          <input
+            name="deliveryOption"
+            type="radio"
+            value={option4}
+            checked={deliveryOption === option4}
+            onChange={handleDeliveryOptionChange}
+          /> {option4} <br /><br />
 
-            <input /> Номер відділення<br />
+          <input
+            name="branchNumber"
+            value={user.branchNumber}
+            onChange={handleChange}
+            hidden={deliveryOption === option4}
+            size="1"
+            />
+          <label hidden={deliveryOption === option4}> Номер відділення Нової Пошти<br /></label>
+          <input
+            name="address"
+            value={user.address}
+            onChange={handleChange}
+            hidden={deliveryOption === option3}
+            />
+          <label hidden={deliveryOption === option3}> Адреса для доставки курьєром<br /></label>
 
-          </form>
-        </article>
+          <input
+            name="city"
+            value={user.city}
+            onChange={handleChange}
+            list="cities"
+            />
+          <label> Город<br /></label>
+          <datalist id="cities">
+            <option value="Львів" />
+            <option value="Київ" />
+          </datalist>
+          <input
+            name="region"
+            value={user.region}
+            onChange={handleChange}
+            />
+          <label> Область<br /></label>
+        </div>
+      </form>
+    </article>
   );
 
 }
