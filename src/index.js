c1b8bcee53b7d5bcbc86c0636f95807660d4ad30
Vladimir Sevastianov <108.nimay@gmail.com>
2021-05-27 13:13:11 +0300

    feat: react-snap

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 //import reportWebVitals from './reportWebVitals';
+import { hydrate, render } from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById("root");
+
+if (rootElement.hasChildNodes()) { // …Если в корневом элементе есть контент, то…
+  hydrate(<App />, rootElement); // …"цепляем" приложение на существующий DOM.
+} else { // …Иначе рендерим приложение стандартным образом
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter><App /></BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+}
