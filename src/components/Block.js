e90f4bc65837971bfa8ae0faff73d9f160be3d0e
Vladimir Sevastianov <108.nimay@gmail.com>
2021-06-17 12:40:52 +0300

    feat: availability

--- src/components/Block.js
+++ src/components/Block.js
@@ -23,20 +23,24 @@ const Block = () => {
           return 'block';
       }
     }
-    return (
-      <article className={blockClassName()} key={key}>
-      <img src={value.img640} alt={`${context.ua ? value.category + ' ' + value.title : value.categoryRu + ' ' + value.titleRu}`} className="product_img" />
-        <div className="title">
-          <img src={value.img} alt={`${value.title}`} className="float" />
-          <h2>{context.ua ? value.category : value.categoryRu} <br /><strong>{context.ua ? value.title : value.titleRu}</strong></h2>
-        </div>
-        <table className="table center">
-          <tbody>
-            <Tr prices={value.price} id={key} />
-          </tbody>
-        </table>
-      </article>
-    );
+
+    if (value.available) {
+      return (
+        <article className={blockClassName()} key={key}>
+        <img src={value.img640} alt={`${context.ua ? value.category + ' ' + value.title : value.categoryRu + ' ' + value.titleRu}`} className="product_img" />
+          <div className="title">
+            <img src={value.img} alt={`${value.title}`} className="float" />
+            <h2>{context.ua ? value.category : value.categoryRu} <br /><strong>{context.ua ? value.title : value.titleRu}</strong></h2>
+          </div>
+          <table className="table center">
+            <tbody>
+              <Tr prices={value.price} id={key} />
+            </tbody>
+          </table>
+        </article>
+      );
+    }
+
 })};
 
 export default Block;
